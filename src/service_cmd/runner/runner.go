6322aa393769249c9f2129460e3784ebb56c6f9d
Sandeep Naik <sandeepnaik50@gmail.com>
2024-10-30 18:07:38 +1100

    dummy change

--- src/service_cmd/runner/runner.go
+++ src/service_cmd/runner/runner.go
@@ -171,7 +171,7 @@ func (runner *Runner) Run() {
 
 	srv.AddDebugHttpEndpoint(
 		"/rlconfig",
-		"print out the currently loaded configuration for debugging",
+		"print out the currently loaded config for debugging",
 		func(writer http.ResponseWriter, request *http.Request) {
 			if current, _ := service.GetCurrentConfig(); current != nil {
 				io.WriteString(writer, current.Dump())
